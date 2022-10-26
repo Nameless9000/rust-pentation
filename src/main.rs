2e4a1e8c2848e6186b282bbeca3dd95837e81d7b
Nameless9000 <dev@name1ess.ga>
2022-10-26 18:44:34 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+fn float_loop(start: f64, threshold: f64, step_size: f64) -> impl Iterator<Item = f64> {
+    std::iter::successors(Some(start), move |&prev| {
+        let next = prev + step_size;
+        (next < threshold).then_some(next)
+    })
+}
+
+fn tetration(a: f64, b: f64) -> f64 {
+    let mut result: f64 = 1.0;
+
+    for _n in float_loop(0.0, b, 1.0) {
+        result = a.powf(result);
+    }
+
+    result
+}
+
+fn petration(a: f64, b: f64) -> f64 {
+    let mut result: f64 = 1.0;
+
+    for _n in float_loop(0.0, b, 1.0) {
+        result = tetration(a, result);
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let output = petration(2.0, 2.0).to_string();
+    println!("Result: {}", output);
 }
