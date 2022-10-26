7b2d84a007f44fe07f6d46937688c6594ba070bc
Nameless9000 <dev@name1ess.ga>
2022-10-26 19:17:27 +0100

    tetration support

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn tetration(a: f64, b: f64) -> f64 {
     result
 }
 
-fn petration(a: f64, b: f64) -> f64 {
+fn pentation(a: f64, b: f64) -> f64 {
     let mut result: f64 = 1.0;
 
     for _n in float_loop(0.0, b, 1.0) {
@@ -29,15 +29,22 @@ fn petration(a: f64, b: f64) -> f64 {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        return println!("Usage: rust-pentation <a: f64> <b: f64>")
+    if args.len() != 4 {
+        return println!("Usage: rust-pentation <type: 1(tetration) | 2(pentation)> <a: f64> <b: f64>")
     }
 
-    let a = args[1].parse::<f64>().unwrap();
-    let b = args[2].parse::<f64>().unwrap();
+    let operation = args[1].parse::<i32>().unwrap();
+
+    let a = args[2].parse::<f64>().unwrap();
+    let b = args[3].parse::<f64>().unwrap();
     
+    let result;
 
-    let output = petration(a, b).to_string();
+    match operation {
+        1=>result = tetration(a, b).to_string(),
+        2=>result = pentation(a, b).to_string(),
+        _=>return println!("Usage: rust-pentation <type: 1(tetration) | 2(pentation)> <a: f64> <b: f64>"),
+    }
 
-    println!("{}[5]{} = {}", a, b, output);
+    println!("{}[{}]{} = {}", a, 3+operation, b, result);
 }
